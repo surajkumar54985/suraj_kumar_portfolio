98780762cd171d8a9bbfe220e77c0ab350e5e7f4
surajkumar54985 <68062925+surajkumar54985@users.noreply.github.com>
2021-05-21 23:41:01 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -60,7 +60,7 @@ for(var i=0 ; i<navMenuAnchorTags.length ; i++)
 }*/
 
 
-var progressbar = document.querySelectorAll('.skill-progress > div');
+/*var progressbar = document.querySelectorAll('.skill-progress > div');
 var skillscontainer = document.getElementById('skills-container');
 window.addEventListener('scroll' , checkscroll);
 
@@ -118,4 +118,69 @@ function checkscroll()
     }
 
 
+}*/
+
+
+
+
+
+
+
+
+
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+
+
+
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
 }
+
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+
+
+
+function fillBar(bar) {
+
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data-bar-width");
+    var interval = setInterval(function () {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+
+}
+
+
+
+// This function uses a for loop for individual progress bars.
+function checkScroll() {
+
+    for (let bar of progressBars) {
+        var barCoordinates = bar.getBoundingClientRect();
+        if ((bar.getAttribute("data-visited") == "false") &&
+            (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        } else if (barCoordinates.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
